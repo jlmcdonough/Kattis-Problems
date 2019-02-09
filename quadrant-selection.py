1720a76735ac70959dfa2b272b57e2a473758143
Joseph McDonough <42847035+jlmcdonough@users.noreply.github.com>
2019-02-08 21:29:46 -0500

    https://open.kattis.com/problems/quadrant

---
+++ quadrant-selection.py
@@ -0,0 +1,10 @@
+p1 = int(input())
+p2 = int(input())
+if (p1>0 and p2>0):
+    print(1)
+elif (p1<0 and p2>0):
+    print(2)
+elif (p1<0 and p2<0):
+    print(3)
+elif (p1>0 and p2<0):
+    print(4)
